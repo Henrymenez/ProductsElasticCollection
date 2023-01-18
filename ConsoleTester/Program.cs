8c53d17e1a799aa78b9094ed39a370526f5d05a5
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2023-01-18 21:33:46 +0100

    Add project files.

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleTester
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
