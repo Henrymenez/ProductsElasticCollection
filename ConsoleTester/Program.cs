d6356a63e1ea0c8adbafff657ed64767a515d104
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2023-01-19 11:27:42 +0100

    updated the product model, added products and return all at search

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -1,11 +1,38 @@
-﻿namespace ConsoleTester
+﻿using System;
+using ElasticCollection.Data;
+
+namespace ConsoleTester
 {
     internal class Program
     {
-
+        public static List<Product> products = new List<Product>();
+       public static void displayAll()
+        {
+            foreach (Product product in products )
+            {
+                Console.WriteLine($"Product Id: {product.Id}, Name: {product.Name},Quantity: {product.Quantity}," +
+                    $"Pric: ${product.Price},Category: {product.Category},OrderCount: {product.OrderCount}");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            products.Add(new Product(1, "Dell xps", 30, 1500, "PCs", 1000));
+            products.Add(new Product(2, "Ergonomic Chair", 400, 200, "Chairs", 4000));
+            products.Add(new Product(3, "Table", 500, 250, "Tables", 3000));
+
+            Console.WriteLine("Weclome to our store \n 1) To display all the products \n" +
+                " 2) To select Order Yourself ");
+            string choiceOne = Console.ReadLine();
+
+            switch(choiceOne)
+            {
+                case "1":
+                    displayAll();
+                    break;
+            }
+
+
         }
     }
 }
---
+++ ElasticCollection/Data/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticCollection.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public int OrderCount { get; set; }
+
+        public Product(int id, string name, int quantity, double price, string category, int ordercount)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+            Category = category;
+            OrderCount = ordercount;
+        }
+    }
+}
